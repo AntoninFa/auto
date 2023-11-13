bb5e40016dbb67492834b2a73fff17674b39e91a
HuemaYilmaz <102851390+HuemaYilmaz@users.noreply.github.com>
2023-11-13 14:32:11 +0100

    deleted just need postgres

--- .extras/db/mysql/sql/log.sql
+++
@@ -1,7 +0,0 @@
--- docker compose exec mysql sh
--- mysql --user=root --password=p < /sql/log.sql
-
--- https://dev.mysql.com/doc/refman/8.1/en/log-file-maintenance.html
--- /var/lib/mysql/mysql.log
-SET GLOBAL general_log = 'ON';
-SET GLOBAL slow_query_log = 'ON';
