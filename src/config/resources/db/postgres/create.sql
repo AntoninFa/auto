21e5281b5edc3dc1967cc3dc97b396a39034c71a
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2023-11-13 22:37:23 +0100

    edited table in create.sql

--- src/config/resources/db/postgres/create.sql
+++ src/config/resources/db/postgres/create.sql
@@ -7,13 +7,14 @@ CREATE TYPE herstellerType AS ENUM ('VOLKSWAGEN', 'AUDI', 'DAIMLER', 'RENAULT');
 CREATE TABLE IF NOT EXISTS auto (
     id                  integer GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY USING INDEX TABLESPACE autospace,
     version             integer NOT NULL DEFAULT 0,
+    modellbezeichnung   varchar(40) NOT NULL,
     hersteller          herstellerType,
     fin                 varchar(17) NOT NULL UNIQUE USING INDEX TABLESPACE autospace,
     kilometerstand      integer NOT NULL CHECK (kilometerstand >= 0),
     auslieferungstag    date,
     grundpreis          decimal(8,2) NOT NULL,
     istAktuellesModell  boolean NOT NULL DEFAULT TRUE,
-    getriebeArt            getriebeType,
+    getriebeArt             getriebeType,
     erzeugt             timestamp NOT NULL DEFAULT NOW(),
     aktualisiert        timestamp NOT NULL DEFAULT NOW()
 ) TABLESPACE autospace;
@@ -22,14 +23,14 @@ CREATE TABLE IF NOT EXISTS ausstattung (
     id                  integer GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY USING INDEX TABLESPACE autospace,
     bezeichnung         varchar(32) NOT NULL,
     preis               decimal(8,2) NOT NULL,
-    verfügbar           boolean NOT NULL DEFAULT TRUE,
+    verfuegbar          boolean NOT NULL DEFAULT TRUE,
     auto_id             integer NOT NULL REFERENCES auto
 ) TABLESPACE autospace;
 CREATE INDEX IF NOT EXISTS ausstattung_auto_id_idx ON ausstattung(auto_id) TABLESPACE autospace;
 
 CREATE TABLE IF NOT EXISTS eigentuemer (
     id                  integer GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY USING INDEX TABLESPACE autospace,
-    eigentuemer                varchar(40) NOT NULL,
+    eigentuemer         varchar(40) NOT NULL,
     geburtsdatum        date,
     fuehrerscheinnummer varchar(40),
     auto_id             integer NOT NULL UNIQUE USING INDEX TABLESPACE autospace REFERENCES auto
