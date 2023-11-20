92fb94b1937e75e7d77dfecf7980877572b9f581
AntoninFa <93150870+AntoninFa@users.noreply.github.com>
2023-11-20 10:25:05 +0100

    Config files and small bugfixes

---
+++ src/security/http/helmet.handler.ts
@@ -0,0 +1,35 @@
+
+/**
+ * Dieses Modul enthält Security-Funktionen für z.B. CSP, XSS, Click-Jacking,
+ * HSTS und MIME-Sniffing, implementiert von helmet.
+ * @packageDocumentation
+ */
+
+import {
+    contentSecurityPolicy,
+    frameguard,
+    hidePoweredBy,
+    hsts,
+    noSniff,
+    xssFilter,
+} from 'helmet';
+
+/**
+ * Security-Funktionen.
+ */
+export const helmetHandlers = [
+    contentSecurityPolicy({
+        useDefaults: true,
+        directives: {
+            defaultSrc: ["https: 'self'"],
+            scriptSrc: ["https: 'unsafe-inline' 'unsafe-eval'"],
+            imgSrc: ["data: 'self'"],
+        },
+        reportOnly: false,
+    }),
+    xssFilter(),
+    frameguard(),
+    hsts(),
+    noSniff(),
+    hidePoweredBy(),
+];
